85b791e92d0c56cbc7fe7509b3d126a18afceddc
ibrahimmohammed11 <ibrahimmohammed9979@gmail.com>
2021-03-15 17:08:09 +0200

    upload project

---
+++ js/main.js
@@ -0,0 +1,64 @@
+
+/* navbar change color  */
+$(window).scroll(function () {
+    let top = $(window).scrollTop();
+    if (top > 400) {
+        $(".navbar").css({
+            "background-color": "#00c3da",
+            "transition": "all 1.5s"
+        })
+    }
+    else {
+        $(".navbar").css({
+            "background-color": "#01142F",
+            "transition": "all 1.5s"
+        })
+    }
+})
+/* navbar change color  */
+/* nav-links smooth scroll  */
+$(".nav-link").click(function () {
+    let currentLink = $(this).attr("href");
+    let secOffset = $(currentLink).offset().top;
+    $("body,html").animate({ scrollTop: secOffset }, 1500)
+})
+/* nav-links smooth scroll  */
+$("#toTop").click(function () {
+    $("body,html").animate({ scrollTop: 0 }, 1500)
+});
+
+$(window).scroll(function () {
+    let top = $(window).scrollTop();
+    if (top > 700) {
+        $("#toTop").fadeIn(1000)
+    }
+    else {
+        $("#toTop").fadeOut(1000)
+    }
+})
+/* back to top button  */
+/* loading screen  */
+$(document).ready(function () {
+    $("#loading").fadeOut(2500, function () {
+        $("body").css("overflow", "visible")
+    })
+})
+/* loading screen  */
+
+/* AOS animation  */
+AOS.init();
+/* AOS animation  */
+/* Date  */
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+today = `${dd} / ${mm} / ${yyyy}`;
+document.getElementById("today").innerHTML=today;
+/* Date  */
+/* carousel  */
+$('.carousel').carousel({
+    interval: 1500
+  })
+/* carousel  */
